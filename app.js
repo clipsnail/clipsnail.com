02b17e70ebb46dd9d8d77a79ee56b43c27b7afab
Stuart P. Bentley <stuart@testtrack4.com>
2013-05-18 22:58:40 -0400

    Add test page form for test clipping

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 function apiapp (db){
   var app = express();
-  
+
   var clips = db.collection('clips');
-  
+
   app.post('/clip', function(req,res,next) {
     var password = req.body.password;
     if(password == process.env.API_TEST_PASSWORD){
@@ -16,10 +16,10 @@ function apiapp (db){
           if(err) return next(err);
         });
     } else {
-      res.send(403,'If you want to insert a clip, you must speak the secret word')
+      res.send(403,'If you want to insert a clip, you must speak the secret word');
     }
   });
-  
+
   return app;
 }
 
@@ -35,12 +35,20 @@ var clips = db.collection('clips');
 app.use(express.static(__dirname+'/www'));
 
 app.use('/api/v0',apiapp(db));
+app.get('/api/v0/clip',function(req, res, next){
+  res.render('manuclip.jade');
+});
 
 app.get('/:user',function(req, res, next){
-  clips.find({user: req.params.user}, function(err,userclips) {
-    if(err) return next(err);
-    res.render('userclips.jade',{clips: userclips});
-  })
+  clips.find({user: req.params.user}, function(err,cursor) {
+    if (err) return next(err);
+    else cursor.toArray(function(err,userclips){
+      if (err) return next(err);
+      //TODO: Check for user entry, not user clips
+      else if (userclips.length == 0) return next();
+      else res.render('userclips.jade',{clips: userclips});
+    });
+  });
 });
 app.get('/:user/tags/:tag',function(req,res){});
 app.get('/:user/:page',function(req,res){});
