fd717d51163b86c707ad68caed2f9b9c6e9df339
Stuart P. Bentley <stuart@testtrack4.com>
2013-06-26 00:08:40 -0400

    Update main page a little

--- app.js
+++ app.js
@@ -8,19 +8,27 @@ function apiapp (db){
   app.use(express.bodyParser());
   app.post('/clip', function(req,res,next) {
     var password = req.body.password;
-    if(password == process.env.API_TEST_PASSWORD){
-      var user = req.body.user || 'test~proto';
+    var user = req.body.user;
+    if(password != process.env.API_TEST_PASSWORD){
+      res.set('Content-Type', 'text/plain').send(403,
+        'If you want to insert a clip, you must speak the secret word');
+    } else if(!user){
+      res.set('Content-Type', 'text/plain').send(403,
+        'Clips need to be attached to a given username' );
+    } else {
+      user = 'test~'+user;
       var html = req.body.html || '';
       var desc = req.body.desc || '';
-      clips.insert({user: user, html: html, desc: desc}, {safe: true},
+      // NOTE: In a proper API, this should be set by the ID of the API
+      // consumer corresponding to the key used to authenticate the method
+      var source = req.body.source || 'anonymous';
+      clips.insert({user: user, html: html.trim(),
+        desc: desc.trim(), source: source}, {safe: true},
         function(err,doc) {
           if(err) return next(err);
           res.set('Content-Type', 'text/plain')
             .send(200,'Inserted:\n\n'+JSON.stringify(doc));
         });
-    } else {
-      res.set('Content-Type', 'text/plain').send(403,
-        'If you want to insert a clip, you must speak the secret word');
     }
   });
 
